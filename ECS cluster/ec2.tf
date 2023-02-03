9ace12f9202ca2dacf97079f2147c466724ce589
Malika85 <malikakh0619@gmail.com>
2023-02-03 03:42:01 +0000

    ecs cluster

---
+++ ECS cluster/ec2.tf
@@ -0,0 +1,15 @@
+resource "aws_instance" "web" {
+  ami           = "ami-0aa7d40eeae50c9a9"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "nginx-web-server"
+  }
+  
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo apt-get update
+    sudo apt-get install -y nginx
+  EOF
+
+}
